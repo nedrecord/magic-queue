6d031f4bcd1e0c06ab8e7355e856ceda806d417b
nedrecord <nedrecord@gmail.com>
2025-12-07 17:23:14 -0800

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+// public/sw.js
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// No fetch handler yet – we’re not doing offline caching right now.
